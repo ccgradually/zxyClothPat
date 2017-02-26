379a953dcb84e60c04014f93b437028c3c2d1bf1
ccgradually <318526670@qq.com>
2017-02-26 15:05:17 +0800

    add cc
    cc join

---
+++ Runtime/Cache/041dea00185f47bccc15092705000b10dd2e3125.file.bottom.html.php
@@ -0,0 +1,62 @@
+<?php /* Smarty version Smarty-3.1.6, created on 2017-02-07 15:23:21
+         compiled from "D:/XAMPP/htdocs/zxyClothPat/Home/View\common\bottom.html" */ ?>
+<?php /*%%SmartyHeaderCode:32572589975e9bd1f73-42338851%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '041dea00185f47bccc15092705000b10dd2e3125' => 
+    array (
+      0 => 'D:/XAMPP/htdocs/zxyClothPat/Home/View\\common\\bottom.html',
+      1 => 1486449305,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '32572589975e9bd1f73-42338851',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.6',
+  'unifunc' => 'content_589975e9c12f8',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_589975e9c12f8')) {function content_589975e9c12f8($_smarty_tpl) {?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>首页</title>
+	<style type="text/css">
+		.bottom{
+			height: 200px;
+			width: 100%;
+			margin-top:40px; 
+			/*background: #fff;*/
+			border-top: 1px solid rgba(204, 204, 204, 0.3);
+			box-shadow: 2px 2px 15px rgba(204, 204, 204, 0.35);		
+		}
+		.b_btn{
+			margin: 0px auto;
+			height: 200px;
+			width: 1080px;
+			/*border: 1px solid black;*/
+
+		}
+		.b_btn li{
+			float: left;
+			width: 269px;
+			height: 150px;
+			list-style: none;
+			border-right: 1px solid rgba(204, 204, 204, 0.7);
+		}
+	</style>
+</head>
+<body>
+	<div class="bottom">	
+		<div class="b_btn">			
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+		</div>
+	</div>
+</body>
+</html><?php }} ?>
---
+++ Runtime/Cache/989b0465ac8c6c96fdbe9501fe0450b3557b74e2.file.bottom2.html.php
@@ -0,0 +1,40 @@
+<?php /* Smarty version Smarty-3.1.6, created on 2017-02-07 15:39:06
+         compiled from "D:/XAMPP/htdocs/zxyClothPat/Home/View\Common\bottom2.html" */ ?>
+<?php /*%%SmartyHeaderCode:115955899799a03d2c7-75284631%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '989b0465ac8c6c96fdbe9501fe0450b3557b74e2' => 
+    array (
+      0 => 'D:/XAMPP/htdocs/zxyClothPat/Home/View\\Common\\bottom2.html',
+      1 => 1486449305,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '115955899799a03d2c7-75284631',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.6',
+  'unifunc' => 'content_5899799a06587',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5899799a06587')) {function content_5899799a06587($_smarty_tpl) {?><!DOCTYPE html>
+<html>
+	<meta charset="utf-8">
+	<title>首页</title>
+	<style type="text/css">
+		.bottom{
+			height: 50px;
+			width: 100%;
+			margin-top:40px; 
+			border-top: 1px solid rgba(204, 204, 204, 0.7);
+			box-shadow: 2px 2px 15px rgba(204, 204, 204, 0.35);		
+		}
+	</style>
+</head>
+<body>
+	<div class="bottom">	
+	</div>
+</body>
+</html><?php }} ?>
